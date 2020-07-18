784a1b50410b90edd5d0dc01d6bdd0899a2c16b7
sillyhat <xushikuan1@163.com>
2020-07-18 23:51:07 +0800

    看不懂……

---
+++ src/x/solutions/Chapter01/Chapter_1_1_IsUnique.java
@@ -0,0 +1,20 @@
+package x.solutions.Chapter01;
+
+public class Chapter_1_1_IsUnique {
+
+    private static boolean IsUnique(String str) {
+        int checker = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int val = str.charAt(i) - 'a';
+            if ((checker & (i << val)) > 0) {
+                return false;
+            }
+            checker |= (1 << val);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(IsUnique("asdglkhsdfghjdsg"));
+    }
+}
